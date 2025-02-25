dc5bbd30145353cac7bd4fd37aa99b7ceeb17fe8
changxing <1278729001@qq.com>
2025-02-25 22:13:07 +0800

    单元测试：配置 GoogleTest 测试环境

---
+++ test/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+TEST(add, zero) {
+    EXPECT_EQ(0, add(0, 0)) << "yes";
+}
+
+TEST(add, positive_number) {
+    EXPECT_EQ(3, add(1, 2));
+}
+
+TEST(add, negative_number) {
+    EXPECT_EQ(-3, add(-1, -2));
+}
+
+int main() {
+    ::testing::InitGoogleTest();
+    return RUN_ALL_TESTS();
+}
